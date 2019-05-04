459f962c31f973ac39b37fd171fd9dfe7b388a29
osamaahmed17 <osamaahmed17@hotmail.com>
2019-05-05 00:33:18 +1000

    Commit 10: User Authentication

--- auth/verify.js
+++ auth/verify.js
@@ -1,26 +1,26 @@
-var jwt = require('jsonwebtoken'); 
-var dotenv = require('dotenv');
-dotenv.load();
 
-exports.verifyUser = function (req, res, next) {
-    var token = req.headers.authorization;
-    
-    if(!token) {
-        return res.status(401).json({ error: 'No token provided.' });
-    }
-    
-    tokenParts = token.split(' ');
-    
-    if(tokenParts[0] == 'Bearer' && tokenParts[1]) {
-        jwt.verify(tokenParts[1],  process.env.SECRET_KEY, function(err, decoded) {
-            if(!err) {
-                req.decoded = decoded;
-                next();
-            } else {
-                return res.status(401).json({ error: 'Authentication failed.' });
-            }
-        });
-    } else {
-        return res.status(401).json({ error: 'Invalid token.' });
-    }
+const jwt = require('jsonwebtoken');
+const secret = 'mysecretsshhh';
+
+const withAuth = function(req, res, next) {
+  const token = 
+      req.body.token ||
+      req.query.token ||
+      req.headers['x-access-token'] ||
+      req.cookies.token;
+
+  if (!token) {
+    res.status(401).send('Unauthorized: No token provided');
+  } else {
+    jwt.verify(token, secret, function(err, decoded) {
+      if (err) {
+        res.status(401).send('Unauthorized: Invalid token');
+      } else {
+        req.email = decoded.email;
+        next();
+      }
+    });
+  }
 }
+
+module.exports = withAuth;
--- controllers/userController.js
+++ controllers/userController.js
@@ -3,8 +3,8 @@ var expressJoi = require('express-joi-validator');
 var jwt = require('jsonwebtoken');
 var express = require("express");
 var user = require('../models/user');
-var verify = require('../auth/verify');
 var router = express.Router();
+const withAuth = require('../auth/verify');
 const AccessToken = require('twilio').jwt.AccessToken;
 const VideoGrant = AccessToken.VideoGrant;
 
@@ -17,11 +17,11 @@ const twilioApiKey = 'SK8f26003fdfb220f3b3a7af42c57f1f12';
 const twilioApiSecret = 'N9ptLeTFAnNy33ontLmYXyZbzcum4wYs';
 const videoGrant = new VideoGrant({
   room: 'cool room',
-  });
+});
 const twilioToken = new AccessToken(twilioAccountSid, twilioApiKey, twilioApiSecret);
 
 /*----------------------------------------------------------------------------------------------*/
-
+const secret = 'mysecretsshhh';
 
 
 
@@ -33,22 +33,22 @@ try {
   vcapLocal = require('../vcap-local.json');
   console.log("Loaded local VCAP", vcapLocal);
 } catch (e) { }
-const appEnvOpts = vcapLocal ? { vcap: vcapLocal} : {}
+const appEnvOpts = vcapLocal ? { vcap: vcapLocal } : {}
 const appEnv = cfenv.getAppEnv(appEnvOpts);
 var Cloudant = require('@cloudant/cloudant');  // Load the Cloudant library.
 if (appEnv.services['cloudantNoSQLDB'] || appEnv.getService(/cloudant/)) {
   if (appEnv.services['cloudantNoSQLDB']) {    // Initialize database with credentials
     cloudant = Cloudant(appEnv.services['cloudantNoSQLDB'][0].credentials);  // CF service named 'cloudantNoSQLDB'
   } else {
-     cloudant = Cloudant(appEnv.getService(/cloudant/).credentials);   // user-provided service with 'cloudant' in its name
+    cloudant = Cloudant(appEnv.getService(/cloudant/).credentials);   // user-provided service with 'cloudant' in its name
   }
-} else if (process.env.CLOUDANT_URL){
+} else if (process.env.CLOUDANT_URL) {
   cloudant = Cloudant(process.env.CLOUDANT_URL);
 }
-if(cloudant) {
+if (cloudant) {
   var dbName = 'mydb';    //database name
-  cloudant.db.create(dbName, function(err, data) {   // Create a new "mydb" database.
-    if(!err) //err if database doesn't already exists
+  cloudant.db.create(dbName, function (err, data) {   // Create a new "mydb" database.
+    if (!err) //err if database doesn't already exists
       console.log("Created database: " + dbName);
   });
   mydb = cloudant.db.use(dbName);     // Specify the database we are going to use (mydb)...
@@ -59,84 +59,106 @@ if(cloudant) {
 /*------------------------------All User Routes and Configuration--------------------------------*/
 
 /*------------------------------For Signin/Login--------------------------------*/
-router.post('/users/signin', function(req, res, next) {
-    mydb.find({
-        selector: { _id: req.body._id}
-    }, function(err, body) {
-        if(!err) {
-            var user = body.docs[0];
-            if(user && req.body.password==user.password) {
-                var payload = {
-                  _id:req.body._id,
-                  username:req.body.username,
-                  password:req.body.password,
-                  fullname:req.body.fullname,
-                  country:req.body.country,
-                  twilioToken:twilioToken.toJwt()
-                };
-                
-                var token = jwt.sign(payload, process.env.SECRET_KEY, {
-                    expiresIn: 3600
-                });
-                
-                return res.status(200).json({
-                    token: token
-                });
-            } else {
-                return res.status(401).json({ error: 'Incorrect id or password.' });
-            }
-        } else {
-            return res.status(401).json({ error: 'Authentication failed.' });
-        }
-    });
-  });
-  /*----------------------------------------------------------------------------------------------*/
-
-
-  /*------------------------------------For Dashboard--------------------------------*/
-  router.get('/users', function(req, res, next){
-    mydb.find({
-        selector: { schema: 'User' }
-    }, function(err, body) {
-        if(!err) {
-            return res.send(body.docs);
-            
-        } else {
-            return res.status(400).json({ error: err });
-            ;
-            
-        }
-    });
-  });
- /*----------------------------------------------------------------------------------------------*/
-
-
-  /*------------------------------For SiginUp-----------------------------------------------------*/
-  router.post('/users/signup', expressJoi(user), function(req, res, next) {
-    twilioToken.identity = req.body.firstname;
-    twilioToken.addGrant(videoGrant);
-    mydb.insert({
-       _id:req.body._id,
-        username:req.body.username,
-        password:req.body.password,
-        fullname:req.body.fullname,
-        country:req.body.country,
-        twilioToken:twilioToken.toJwt(),
-        schema: 'User'
-    }, function(err, body) {
-        if(!err) {
-            return res.status(200).json({
-              _id:req.body._id,
-              username:req.body.username,
-              password:req.body.password,
-              fullname:req.body.fullname,
-              country:req.body.country,
-              twilioToken:twilioToken.toJwt()
+router.post('/api/authenticate', function (req, res, next) {
+  mydb.find({
+    selector: { _id: req.body._id }
+  }, function (err, user) {
+    if (err) {
+      console.error(err);
+      res.status(500)
+        .json({
+          error: 'Internal error please try again'
+        });
+    } else if (!user) {
+      res.status(401)
+        .json({
+          error: 'Incorrect email or password'
+        });
+    } else {
+      user.isCorrectPassword(password, function (err, same) {
+        if (err) {
+          res.status(500)
+            .json({
+              error: 'Internal error please try again'
+            });
+        } else if (!same) {
+          res.status(401)
+            .json({
+              error: 'Incorrect email or password'
             });
         } else {
-            return next(err);
+          // Issue token
+          var payload = {
+            _id: req.body._id,
+            username: req.body.username,
+            password: req.body.password,
+            fullname: req.body.fullname,
+            country: req.body.country,
+            twilioToken: twilioToken.toJwt()
+          };;
+          const token = jwt.sign(payload, secret, {
+            expiresIn: '1h'
+          });
+          res.cookie('token', token, { httpOnly: true }).sendStatus(200);
         }
-    });
+      });
+    }
   });
- /*----------------------------------------------------------------------------------------------*/
-  module.exports = router;
+});
+/*----------------------------------------------------------------------------------------------*/
+
+
+/*------------------------------------For Dashboard--------------------------------*/
+router.get('/users', function (req, res, next) {
+  mydb.find({
+    selector: { schema: 'User' }
+  }, function (err, body) {
+    if (!err) {
+      return res.send(body.docs);
+
+    } else {
+      return res.status(400).json({ error: err });
+      ;
+
+    }
+  });
+});
+
+
+/*-----------------------------For Checking Authentication------------------------------------------*/
+router.get('/checkToken', withAuth, function (req, res) {
+  res.sendStatus(200);
+});
+
+/*----------------------------------------------------------------------------------------------*/
+
+
+/*------------------------------For SiginUp-----------------------------------------------------*/
+router.post('/users/signup', expressJoi(user), function (req, res, next) {
+  twilioToken.identity = req.body.firstname;
+  twilioToken.addGrant(videoGrant);
+  mydb.insert({
+    _id: req.body._id,
+    username: req.body.username,
+    password: req.body.password,
+    fullname: req.body.fullname,
+    country: req.body.country,
+    twilioToken: twilioToken.toJwt(),
+    schema: 'User'
+  }, function (err, body) {
+    if (!err) {
+      return res.status(200).json({
+        _id: req.body._id,
+        username: req.body.username,
+        password: req.body.password,
+        fullname: req.body.fullname,
+        country: req.body.country,
+        twilioToken: twilioToken.toJwt()
+      });
+    } else {
+      return next(err);
+    }
+  });
+});
+/*----------------------------------------------------------------------------------------------*/
+module.exports = router;
