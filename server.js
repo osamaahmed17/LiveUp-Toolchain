0683fd9ff9000d1f58cd216db94d2209c327f42d
osamaahmed17 <osamaahmed17@hotmail.com>
2019-05-18 05:12:24 +1000

    Commit 15: Socket Configuration

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-var http=require('http')
-const server=http.createServer(app);
+var http = require('http')
+var router = express.Router();
+const server = http.createServer(app);
 var usersController = require('./controllers/userController');
-var socketIO= require('socket.io');
-var cors=require('cors');
-var io= socketIO(server);
+var socketIO = require('socket.io');
+var cors = require('cors');
+var io = socketIO(server);
 
 
 app.use(cors())
@@ -16,16 +17,16 @@ app.use('/', usersController);
 app.use(express.static(__dirname + '/views'));
 
 
-io.on('connection',socket=>{
-    console.log("its connected")
-    socket.on('greet',greeting=>{
-        console.log(greeting)
-    })
+io.on('connection', socket => {
+    socket.emit("FromAPI", "Osama"); // Emitting a new message. It will be consumed by the client
+    socket.on("disconnect", () => {
+        console.log("Client disconnected");
+      })
 })
 
 
 
-server.listen(process.env.PORT || 3000, function() {
+server.listen(process.env.PORT || 3000, function () {
     console.log("Apps is on localhost:3000");
 
 });
