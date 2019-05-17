0b4781561c7894fbb3e60a86d68e6c06a9aded4d
osamaahmed17 <osamaahmed17@hotmail.com>
2019-05-18 04:46:39 +1000

    Commit 15: Socket Configuration

--- server.js
+++ server.js
@@ -1,45 +1,33 @@
 var express = require("express");
-var cors = require('cors');
 var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-var usersController = require('./controllers/userController');
+var http=require('http')
+const server=http.createServer(app);
+//var usersController = require('./controllers/userController');
+var socketIO= require('socket.io');
+var cors=require('cors');
+var io= socketIO(server);
 
 
-
-app.use(cors({
-    'allowedHeaders': ['Content-Type'], // headers that React is sending to the API
-    'exposedHeaders': ['Content-Type'], // headers that you are sending back to React
-    'origin': '*',
-    'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    'preflightContinue': false
-}));
+app.use(cors())
 app.use('/', usersController);
-
-
 app.use(express.static(__dirname + '/views'));
 
 
+io.on('connection',socket=>{
+    console.log("its connected")
+    socket.on('greet',greeting=>{
+        console.log(greeting)
+    })
+})
 
 
 
-const server = app.listen(process.env.PORT || 3000, function() {
-    console.log("To view your app, open this link in your : http://localhost:" + server);
+server.listen(process.env.PORT || 3000, function() {
+    console.log("Apps is on localhost:3000");
+
 });
-
-
-
-const io = require('socket.io')(server);
-
-// Set socket.io listeners.
-io.on('connection', (socket) => {
-    console.log('a user connected');
-  
-    socket.on('disconnect', () => {
-      console.log('user disconnected');
-    });
-  });
-
 
 module.exports = app;
